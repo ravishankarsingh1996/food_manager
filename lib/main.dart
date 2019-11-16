ac674563e59be7ad48348e44106bb1e6e234d63f
Ravi Shankar Singh <ravi.singh@mobcoder.com>
2019-11-17 02:10:35 +0530

    Tasks Done: 1. Generating QR Code with custom data. 2. Scanning the QR code and displaying the data received from QR code.

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Food Manager'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _scanBarcode ="";
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+
+    barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        "#42f5ef", "Cancel", true, ScanMode.QR);
+    print(barcodeScanRes);
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Text(_scanBarcode),
+              QrImage(
+                  data: "BM27 MobCoder LLC",// this the data part where we need to add employeeID with current date.
+                  version: QrVersions.auto,
+                  size: 400.0,
+                  gapless: false,
+//                  backgroundColor:  Color.fromARGB(255, 200, 100, 100),
+//                  foregroundColor: Color.fromARGB(255, 200, 25, 25),
+                  errorStateBuilder: (cxt, err) {
+                    return Container(
+                      child: Center(
+                        child: Text(
+                          "Uh oh! Something went wrong...",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    );
+                  },
+                  embeddedImage: NetworkImage('http://3.bp.blogspot.com/-EE2J_9N7FdI/Xc-5jf-ssgI/AAAAAAAAXmI/zWxKqrHeKGkOTBZd6aAFeZ5vXCDo6E2cgCK4BGAYYCw/s400/logo_1.png'),
+                  embeddedImageStyle : QrEmbeddedImageStyle(
+                      size: Size.square(70),
+                      color: Color.fromARGB(100, 10, 10, 10)
+                  )
+              ),
+            ],
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: scanBarcodeNormal,
+        tooltip: 'Scan',
+        child: Icon(Icons.search),
+      ),
+    );
+  }
+}
